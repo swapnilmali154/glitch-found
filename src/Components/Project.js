1b445fa5ed4d5c3350d57a411ea69ba0122f2e40
Rutuja <ingawale.rutuja99@example.com>
2024-04-15 19:31:07 +0530

    Navbar UI Design

---
+++ src/Components/Project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Project = () => {
+    return (
+        <div>
+            <h6>Project</h6>
+        </div>
+    );
+};
+
+export default Project;
---
+++ src/Components/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div>
+            <h6>User</h6>
+        </div>
+    );
+};
+
+export default User;
