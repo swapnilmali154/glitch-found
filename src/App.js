7223a7e5411624a90d38d76eea99ca39d27c5f24
Rutuja <ingawale.rutuja99@example.com>
2024-04-15 19:29:47 +0530

    Navbar UI Design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import User from './Components/User';
+import Project from './Components/Project';
 
 function App() {
+  const url = "https://onlinetestapi.gerasim.in/api/Glitch/";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>
+          <div classNameName="App">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#">GlitchFound</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  
+                    <li className="nav-item">
+                      <Link className='nav-link' to="/user">User</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className='nav-link' to="/project">Project</Link>
+                    </li>
+                    
+                   
+                    
+
+                  </ul>
+
+                </div>
+              </div>
+            </nav>
+
+            <Routes>
+              <Route path="user" element={<User></User>}></Route>
+              <Route path="/project" element={<Project></Project>}></Route>
+               
+            </Routes>
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
