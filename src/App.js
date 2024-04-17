4fb42f91792ad5b7fd3ba42c35cfeae3864d28c7
Pooja misal <poojamisal1007@gmail.com>
2024-04-17 19:12:53 +0530

    IssueStatus

--- src/App.js
+++ src/App.js
@@ -41,10 +41,7 @@ function App() {
                     </li>
                     <li className="nav-item">
                       <Link className='nav-link' to="/ticketStatus">TicketStatus</Link>
-                    </li>
-                  
-                   
-                    
+                    </li>  
 
                   </ul>
                 </div>
